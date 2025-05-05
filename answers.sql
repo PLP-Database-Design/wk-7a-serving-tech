e630b8f245118dfe79d2935382776367733b0173
Moses Muiruri <saviamarketing.01@gmail.com>
2025-05-05 14:05:43 +0300

    Update answers.sql resubmission

--- answers.sql
+++ answers.sql
@@ -1,32 +1,36 @@
 ** Question one 
 
--- Create a new table in 1NF
-CREATE TABLE ProductDetail_1NF (
+CREATE TABLE ProductDetail (
     OrderID INT,
     CustomerName VARCHAR(100),
     Product VARCHAR(100)
 );
 
--- Insert data into the new table, splitting the Products column
-INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product)
+INSERT INTO ProductDetail (OrderID, CustomerName, Product)
 VALUES
-    (101, 'John Doe', 'Laptop'),
-    (101, 'John Doe', 'Mouse'),
-    (102, 'Jane Smith', 'Tablet'),
-    (102, 'Jane Smith', 'Keyboard'),
-    (102, 'Jane Smith', 'Mouse'),
-    (103, 'Emily Clark', 'Phone');
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
 
 ** Question Two 
 
--- Create Orders table to store order-related information
 CREATE TABLE Orders (
     OrderID INT PRIMARY KEY,
     CustomerName VARCHAR(100)
 );
 
--- Create OrderDetails_2NF table to store product details
-CREATE TABLE OrderDetails_2NF (
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+
+CREATE TABLE Product (
     OrderID INT,
     Product VARCHAR(100),
     Quantity INT,
@@ -34,12 +38,11 @@ CREATE TABLE OrderDetails_2NF (
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
 
--- Insert data into Orders table
-INSERT INTO Orders (OrderID, CustomerName)
-SELECT DISTINCT OrderID, CustomerName
-FROM OrderDetails;
-
--- Insert data into OrderDetails_2NF table
-INSERT INTO OrderDetails_2NF (OrderID, Product, Quantity)
-SELECT OrderID, Product, Quantity
-FROM OrderDetails;
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
